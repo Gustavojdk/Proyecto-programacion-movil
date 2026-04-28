643ca93275d17944cdd466aa020bbb28c18ced77
Gerald Emanuel Vargas Morales <202405633@est.umss.edu>
2026-04-28 15:59:16 -0400

    homescreen basico

--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -1,10 +1,14 @@
-// En lib/main.dart, antes de MyApp, agrega:
+import 'package:flutter/material.dart';
+
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Home')),
-      body: Center(child: Text('Pantalla de Inicio')),
+    return const Scaffold(
+      body: Center(
+        child: Text('Hola'),
+      ),
     );
   }
 }
